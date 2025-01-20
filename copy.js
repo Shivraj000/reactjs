db45570c0b8dad38f7bc3e299791d0cb9eda5588
Shivraj <142338267+Shivraj000@users.noreply.github.com>
2025-01-20 21:22:07 +0530

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,39 @@
+const Bodyslider =()=>
+    {
+        return (
+            <div className="logo-slider">
+                <div className="logos-slide">
+        
+                <div className="slide">
+                    <img src="https://wallpapers.com/images/high/movie-poster-background-k1bnmu30qdwultpn.webp" alt="Image 1"/>
+                    <h2>Title 1</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 2"/>
+                    <h2>Title 2</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 1"/>
+                    <h2>Title 3</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 2"/>
+                    <h2>Title 3</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 1"/>
+                    <h2>Title 4</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 2"/>
+                    <h2>Title 5</h2>
+                </div>
+              <div className="slide">
+                    <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Image 1"/>
+                    <h2>slider end</h2>
+                </div>
+        
+      </div>
+    </div>);
+        };
+    
